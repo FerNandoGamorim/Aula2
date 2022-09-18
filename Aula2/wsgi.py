b5f81b034d397894c2cd8d2b1372e6f437d5d9ba
DevGamorim <78799375+DevGamorim@users.noreply.github.com>
2022-09-18 11:06:44 -0300

    fix deploy error path

--- Aula2/wsgi.py
+++ Aula2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Config project.
+WSGI config for Aula2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,27 +8,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-import  sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aula2.settings')
 
 application = get_wsgi_application()
-
-# +++++++++++ DJANGO +++++++++++ 
-# Para usar seu próprio aplicativo Django, use um código como este: 
-# supondo que seu arquivo de configurações do Django esteja em '/home/myusername/mysite/mysite/settings.py' 
-path  =  'home/fergamorim/Aula2' 
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Aula2.settings'
-
-## Uncomment the lines below depending on your Django version
-###### then, for Django >=1.5:
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-###### or, for older Django <=1.4
-#import django.core.handlers.wsgi
-#application = django.core.handlers.wsgi.WSGIHandler()
